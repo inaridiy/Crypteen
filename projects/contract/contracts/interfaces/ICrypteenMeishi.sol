ad547dcb30dd59b1e8ab83d03cc813bf732d074b
Inaridiy <inainainariinainari@gmail.com>
2022-07-26 09:12:38 +0000

    create interfaces

---
+++ projects/contract/contracts/interfaces/ICrypteenMeishi.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+
+interface ICrypteenMeishi is IERC721Enumerable {
+  function mint() {}
+}
